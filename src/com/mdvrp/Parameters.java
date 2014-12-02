40066bb7f49b29e5ca1b7a16aecf3ed241d0902b
Marco Ardizzone <kwbmm1990@gmail.com>
2014-12-02 22:55:53 +0100

    Parameters restored
    Some parameters that were commented out, are now restored

--- src/com/mdvrp/Parameters.java
+++ src/com/mdvrp/Parameters.java
@@ -31,40 +31,40 @@ public class Parameters {
 		if(args.length % 2 == 0){
 			for(int i = 0; i < args.length; i += 2){
 				switch (args[i]) {
-//					case "-mT":
-//						movesType = MovesType.SWAP;
-//						break;
+					case "-mT":
+						movesType = MovesType.SWAP;
+						break;
 					case "-if":
 						inputFileName = args[i+1];
 						break;
 					case "-of":
 						outputFileName = args[i+1];
 						break;
-//					case "-p":
-//						precision = Double.parseDouble(args[i+1]);
-//						break;
+					case "-p":
+						precision = Double.parseDouble(args[i+1]);
+						break;
 					case "-it":
 						iterations = Integer.parseInt(args[i+1]);
 						break;
-//					case "-sc":
-//						startClient = Integer.parseInt(args[i+1]);
-//						break;
-//					case "-rs":
-//						randomSeed = Integer.parseInt(args[i+1]);
-//						break;
+					case "-sc":
+						startClient = Integer.parseInt(args[i+1]);
+						break;
+					case "-rs":
+						randomSeed = Integer.parseInt(args[i+1]);
+						break;
 					case "-t":
 						tabuTenure = Integer.parseInt(args[i+1]);
 						break;
-//					case "-vt":
-//						if(args[i+1].equalsIgnoreCase("true")){
-//							setVariableTenure(true);
-//						}else if(args[i+1].equalsIgnoreCase("false")){
-//							setVariableTenure(false);
-//						}else {
-//							System.out.println("Variable tenure argument must be true of false. Set to default false!");
-//							throw new Exception();
-//						}
-//						break;
+					case "-vt":
+						if(args[i+1].equalsIgnoreCase("true")){
+							setVariableTenure(true);
+						}else if(args[i+1].equalsIgnoreCase("false")){
+							setVariableTenure(false);
+						}else {
+							System.out.println("Variable tenure argument must be true of false. Set to default false!");
+							throw new Exception();
+						}
+						break;
 					default: {
 						System.out.println("Unknown type of argument: " + args[i]);
 						throw new Exception();
